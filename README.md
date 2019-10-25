b7f1adf547a674d02516a3aa08796d30f83df911
Jono <jono.davis66@gmail.com>
2019-10-25 15:01:21 +1300

    added readme

---
+++ README.md
@@ -0,0 +1,2 @@
+# dgg-client
+dgg-client is a destiny.gg chat client which runs in the command line. it requires node.js and npm.
--- client.js
+++ client.js
@@ -4,14 +4,35 @@ const log = console.log;
 
 const ws = new WebSocket("wss://www.destiny.gg/ws");
 
+// runs on opening of the websocket
 ws.on("open", function open() {
-  log(chalk.cyan(`Connected to destiny.gg`));
+  log(chalk.cyan(`Connected.`));
 });
 
+// runs on each message received by the websocket
 ws.on("message", function incoming(data) {
+  // FORMAT OF DATA:
+  // MSG
+  // {
+  //   "nick":"bird",
+  //   "features":["subscriber","flair9"],
+  //   "timestamp":1571967272433,
+  //   "data":"NO GODSTINY BURN EVERY BRIDGE PEPE"
+  // }
   const type = data.split(" ")[0];
+  let msg = JSON.parse(data.substring(data.indexOf(" ")));
+  let feats = [];
+  if (type === "NAMES") {
+    // msg.users.forEach(user => {
+    //   user.features.forEach(feat => {
+    //     if (!feats.includes(feat)) {
+    //       feats.push(feat);
+    //     }
+    //   });
+    // });
+    log(`Serving ${chalk.cyan(msg.connectioncount)} connections and ${chalk.cyan(msg.users.length)} users.`)
+  }
   if (type === "MSG") {
-    let msg = JSON.parse(data.substring(data.indexOf(" ")));
     if (msg.features.includes("admin")) {
       log(`${chalk.red.bold(msg.nick)}: ${msg.data}`);
       return;
@@ -21,11 +42,11 @@ ws.on("message", function incoming(data) {
       return;
     }
     if (msg.features.includes("bot")) {
-      log(`🤖  ${chalk.dim(msg.nick)}: ${chalk.dim(msg.data)}`);
+      log(`🤖 ${chalk.dim(msg.nick)}: ${chalk.dim(msg.data)}`);
       return;
     }
     if (msg.features.includes("protected")) {
-      log(`✔  ${chalk.blue.bold(msg.nick)}: ${msg.data}`);
+      log(`✔ ${chalk.blue.bold(msg.nick)}: ${msg.data}`);
       return;
     }
     if (msg.features.includes("subscriber")) {
@@ -35,3 +56,5 @@ ws.on("message", function incoming(data) {
     log(`${chalk.bold(msg.nick)}: ${msg.data}`);
   }
 });
+
+log(chalk.cyan(`Connecting to destiny.gg ...`))
